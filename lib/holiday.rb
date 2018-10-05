b72fa210b0f62550c96eaf77a6575d810ece4bf1
Kelly Sheddy <jungaka07@gmail.com>
2018-10-05 12:08:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ end
 
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall] << {:columbus_day 
+  holiday_hash[:fall] << {:columbus_day supply) 
 
 end
 
